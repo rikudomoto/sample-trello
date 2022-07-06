3278f83c377f3cd8703f7ec2797a4a6171dca2ed
rikudomoto <73264978+rikudomoto@users.noreply.github.com>
2022-07-06 15:43:27 +0900

    header実装

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
+import Header from "./components/header/Header";
+
 function App() {
-  return <div></div>;
+  return <div>
+    <Header />
+  </div>;
 }
 
 export default App;
---
+++ src/components/header/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div>
+      <header><h1>SimpleTrello</h1></header>
+    </div>
+  )
+}
+
+export default Header
