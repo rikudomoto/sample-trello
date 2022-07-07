e83ea82d95448c425ce24bce7eb0832f47948646
rikudomoto <73264978+rikudomoto@users.noreply.github.com>
2022-07-07 10:36:51 +0900

    タスクタイトル実装

--- src/components/task/TaskCardTitle.jsx
+++ src/components/task/TaskCardTitle.jsx
@@ -1,9 +1,43 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const TaskCardTitle = () => {
+  const [isClick, setIsClick] = useState(false);
+  const [inputCardTitle, setInputCardTitle] = useState("Today");
+
+  const handleClick = () => {
+    setIsClick(true);
+    console.log(isClick);
+  };
+
+  const handleChange = (e) => {
+    setInputCardTitle(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setIsClick(false);
+  };
+
+  const handleBlur = () => {
+    setIsClick(false);
+  };
+
   return (
-    <div>
-      
+    <div onClick = {handleClick} className = "taskCardTitleInputArea">
+      {isClick ? 
+      <form onSubmit = {handleSubmit}>
+        <input
+          className='taskCardTitleInput'
+          type="text" 
+          autoFocus
+          onChange = {handleChange}
+          onBlur = {handleBlur} 
+          value = {inputCardTitle}
+          maxLength = {10}
+        />
+      </form>
+      : 
+      <h3>{inputCardTitle}</h3>}
     </div>
   )
 }
