f14155aedab88fb81317f98b48fdf486ab242e0c
rikudomoto <73264978+rikudomoto@users.noreply.github.com>
2022-07-07 17:43:02 +0900

    タスクドロップ実装

--- src/components/task/TaskCard.jsx
+++ src/components/task/TaskCard.jsx
@@ -4,25 +4,30 @@ import TaskAddInput from './input/TaskAddInput'
 import TaskCardTitle from './TaskCardTitle'
 import Tasks from './Tasks'
 import react, {useState} from 'react';
+import { Draggable } from 'react-beautiful-dnd'
 
-const TaskCard = ({taskCardsList, setTaskCardsList, taskCard}) => {
+const TaskCard = ({taskCardsList, setTaskCardsList, taskCard, index}) => {
   const [inputText, setInputText] = useState("");
   const [taskList, setTaskList] = useState([]);
 
   return (
-    <div className='taskCard'>
-      <div className='taskCardTitleAndTaskCardDeleteButtonArea'>
-      <TaskCardTitle />
-      <TaskCardDeleteButton taskCardsList={taskCardsList} setTaskCardsList={setTaskCardsList} taskCard={taskCard}/>
+    <Draggable draggableId = {taskCard.id} index = {index}>
+      {(provided) => (
+        <div className='taskCard' ref={provided.innerRef} {...provided.draggableProps}>
+        <div className='taskCardTitleAndTaskCardDeleteButtonArea' {...provided.dragHandleProps}>
+        <TaskCardTitle />
+        <TaskCardDeleteButton taskCardsList={taskCardsList} setTaskCardsList={setTaskCardsList} taskCard={taskCard}/>
+        </div>
+        <TaskAddInput 
+          inputText = {inputText}
+          setInputText = {setInputText}
+          setTaskList = {setTaskList}
+          taskList = {taskList}
+        />
+        <Tasks inputText = {inputText} taskList = {taskList} setTaskList = {setTaskList}/>
       </div>
-      <TaskAddInput 
-        inputText = {inputText}
-        setInputText = {setInputText}
-        setTaskList = {setTaskList}
-        taskList = {taskList}
-      />
-      <Tasks inputText = {inputText} taskList = {taskList} setTaskList = {setTaskList}/>
-    </div>
+      )}
+    </Draggable>
   )
 }
 
